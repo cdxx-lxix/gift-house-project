0d9dc7d7abfa8e582c375d46803a2494b508cc9c
CDXX-LXIX <taocross666@gmail.com>
2024-06-29 17:12:20 +0200

    mobile version update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="max-w-6xl h-full m-auto moserrat px-20">
+  <main class="max-w-6xl h-full m-auto moserrat px-20 hidden md:block">
     <section class="w-full max-h-[23rem] min-h-[23rem]">
 
       <div class="absolute right-[20%] z-10 w-full inline-flex justify-end items-center gap-8 pt-6 pr-16">
@@ -156,6 +156,177 @@
     </div>
 
   </main>
+
+  <div class="w-full block md:hidden monserrat">
+    <section class="mobile-header px-4">
+      <div class="w-full h-12 inline-flex justify-between items-center">
+
+        <div>
+          <svg class="w-8 h-8" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M2 3h12a1 1 0 0 1 0 2H2a1 1 0 1 1 0-2m0 4h12a1 1 0 0 1 0 2H2a1 1 0 1 1 0-2m0 4h12a1 1 0 0 1 0 2H2a1 1 0 0 1 0-2" />
+          </svg>
+        </div>
+
+        <div class="font-medium text-xl background-blur">
+          devicehouse.pl@gmail.com
+        </div>
+
+        <div>
+          <img class="w-8 h-8" :src="cart" alt="">
+        </div>
+
+      </div>
+
+      <div>
+        <img class="aspect-video w-1/2 mx-auto mt-4" :src="logo" alt="">
+      </div>
+
+      <div class="my-4">
+        <h1 class="text-lg text-center px-8 font-medium text-[#5E4B40] mb-8">САМЫЙ БОЛЬШОЙ
+          ОНЛАЙН-МАГАЗИН
+          ОРИГИНАЛЬНЫХ ПОДАРКОВ
+          С ЛАЗЕРНЫМ ГРАВИРОВАНИЕМ ДЛЯ СЕБЯ, ДРУЗЕЙ, СЕМЬИ, КОЛЛЕГ, ЛЮБИМЫХ.</h1>
+      </div>
+    </section>
+
+    <section class="w-full">
+      <div class="absolute right-[2%] top-[38%]">
+        <img class="w-16 h-16 transform rotate-[-20deg] opacity-40" :src="keychain" alt="lighter icon">
+      </div>
+      <h2 class="text-center text-3xl chibola text-[#2C221C] mb-16">ПОПУЛЯРНЫЕ ТОВАРЫ</h2>
+    </section>
+
+    <section class="w-full inline-flex gap-12 justify-center items-center mt-2 mb-10">
+      <button class="w-14 h-10 bg-gray-50/70 rounded-sm">
+        <svg class="w-12 h-6 m-auto" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M17 9H5.414l3.293-3.293a.999.999 0 1 0-1.414-1.414l-5 5a1 1 0 0 0 0 1.414l5 5a.997.997 0 0 0 1.414 0 1 1 0 0 0 0-1.414L5.414 11H17a1 1 0 1 0 0-2" />
+        </svg>
+      </button>
+
+      <div class="border border-black px-10 py-4">
+        <img class="max-h-[26rem]" :src="store.products[1].img" alt="">
+        <div>
+          <div class="inline-flex justify-between w-full">
+            <div>
+              <p>{{ store.products[1].name }}</p>
+              <p class="text-[#B84500] font-semibold">{{ store.products[1].price }}</p>
+            </div>
+            <button>
+              <img class="w-10 h-10 rounded-full" :src="addtocart" alt="add to cart icon">
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <button class="w-14 h-10 bg-gray-50/70 rounded-sm">
+        <svg class="w-12 h-6 m-auto" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="m17.707 9.293-5-5a.999.999 0 1 0-1.414 1.414L14.586 9H3a1 1 0 1 0 0 2h11.586l-3.293 3.293a.999.999 0 1 0 1.414 1.414l5-5a1 1 0 0 0 0-1.414" />
+        </svg>
+      </button>
+      <div class="absolute left-8 top-[77%]">
+        <img class="w-14 h-14 transform rotate-[-25deg] opacity-60" :src="case_icon" alt="case icon">
+      </div>
+    </section>
+
+    <section class="w-full">
+      <h2 class="text-center text-3xl chibola text-[#2C221C] mb-2">КАТАЛОГ ТОВАРОВ</h2>
+      <div class="w-full inline-flex justify-end pr-5">
+        <img class="w-14 h-14 transform rotate-[25deg] opacity-40" :src="wallet" alt="wallet icon">
+      </div>
+    </section>
+
+    <section class="w-full grid grid-cols-2 gap-12 px-8 mt-4">
+      <div>
+        <img class="absolute inline-flex text-center items-center justify-center w-14 h-14 left-[38%]" :src="sale"
+          alt="">
+        <div class="border border-black p-4 flex flex-col">
+          <img class="max-h-[18rem] object-contain mb-3" :src="store.products[3].img" alt="">
+          <div>
+            <div class="inline-flex justify-between w-full px-1">
+              <div>
+                <p class="text-xs">{{ store.products[3].name }}</p>
+                <p class="text-[#B84500] font-semibold">{{ store.products[3].price }}</p>
+              </div>
+              <button>
+                <img class="w-10 h-10 rounded-full" :src="addtocart" alt="add to cart icon">
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="border border-black p-4 flex flex-col">
+        <img class="max-h-[18rem] object-contain mb-3" :src="store.products[5].img" alt="">
+        <div>
+          <div class="inline-flex justify-between w-full px-1">
+            <div>
+              <p class="text-xs">{{ store.products[5].name }}</p>
+              <p class="text-[#B84500] font-semibold">{{ store.products[1].price }}</p>
+            </div>
+            <button>
+              <img class="w-10 h-10 rounded-full" :src="addtocart" alt="add to cart icon">
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="border border-black p-4 flex flex-col">
+        <img class="max-h-[18rem] object-contain mb-3" :src="store.products[6].img" alt="">
+        <div>
+          <div class="inline-flex justify-between w-full px-1">
+            <div>
+              <p class="text-xs">{{ store.products[6].name }}</p>
+              <p class="text-[#B84500] font-semibold">{{ store.products[1].price }}</p>
+            </div>
+            <button>
+              <img class="w-10 h-10 rounded-full" :src="addtocart" alt="add to cart icon">
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="border border-black p-4 flex flex-col">
+        <img class="max-h-[18rem] object-contain mb-3" :src="store.products[8].img" alt="">
+        <div>
+          <div class="inline-flex justify-between w-full px-1">
+            <div>
+              <p class="text-xs">{{ store.products[8].name }}</p>
+              <p class="text-[#B84500] font-semibold">{{ store.products[1].price }}</p>
+            </div>
+            <button>
+              <img class="w-10 h-10 rounded-full" :src="addtocart" alt="add to cart icon">
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="pt-8">
+      <div class="absolute left-4">
+        <img class="w-14 h-14 transform rotate-[25deg] opacity-40" :src="lighter" alt="lighter icon">
+      </div>
+      <div class="mt-8 mb-8">
+        <img class="aspect-video w-1/3 mx-auto mt-4" :src="logo" alt="">
+      </div>
+      <div class="font-medium text-xl text-center">
+        devicehouse.pl@gmail.com
+      </div>
+    </section>
+
+    <div class="fixed bottom-20 right-[4%] z-50 mt-24 mr-2">
+      <div class="p-2 rounded-full bg-gray-300/90 cursor-pointer opacity-70 border border-black">
+        <svg class="w-8 h-8" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0h48v48H0z" fill="none" />
+          <path d="m6.586 30.585 2.828 2.828L24 18.827l14.586 14.586 2.828-2.828L24 13.171z" />
+        </svg>
+      </div>
+    </div>
+
+  </div>
+
 </template>
 
 <script setup>
@@ -168,6 +339,8 @@ import pen from '../assets/pen.png'
 import loading from '../assets/loading.png'
 import lighter from '../assets/lighter.png'
 import keychain from '../assets/keychain.png'
+import addtocart from '../assets/addtocart.png'
+import sale from '../assets/sale.png'
 import { useBasketStore } from '../stores/basket.js'
 import ProductCard from '../components/ProductCard.vue'
 
@@ -195,6 +368,12 @@ main {
       rgba(0, 0, 0, 0.1)), url('../assets/wooden_pattern.png');
   background-repeat: repeat-y;
   mix-blend-mode: darken;
+}
+
+.mobile-header {
+  background-color: #837960;
+  background-image: linear-gradient(to bottom, transparent, #ffffff49), url('../assets/wooden_pattern.png');
+  background-repeat: no-repeat;
 }
 
 .background-blur {
